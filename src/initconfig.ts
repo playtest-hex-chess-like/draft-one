7a0b94fa120b7a459a7e651f6b3e8aa9ea502fdb
louisjasonsundoro <55065518+louisjasonsundoro@users.noreply.github.com>
2026-03-28 00:47:39 +0700

    Update initconfig.ts

--- src/initconfig.ts
+++ src/initconfig.ts
@@ -1,8 +1,8 @@
 export const BOARD_GAP = 10;
 export const STARTING_RESERVES = 40;
-export const STALL_TIMER_DEFAULT = 5;
+export const STALL_TIMER_DEFAULT = 7;
 export const stallTimerInactiveFirstXTurns = 10;
-export const stallTimerInfoVisible = true;
+export const stallTimerInfoVisible = false;
 
 export const INITIAL_LAYOUT = [
   { q: 0, r: 0, count: 6 },
